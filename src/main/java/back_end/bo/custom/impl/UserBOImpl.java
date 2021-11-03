c299bea0c0abc71f7d6aee4a3acabc82058e1587
dark-sl <charaka.janith.bandarawaththa@gmail.com>
2021-11-03 17:13:02 +0530

    db cleaned

--- src/main/java/back_end/bo/custom/impl/UserBOImpl.java
+++ src/main/java/back_end/bo/custom/impl/UserBOImpl.java
@@ -1,4 +1,14 @@
 package back_end.bo.custom.impl;
 
-public class UserBOImpl {
+import back_end.bo.custom.UserBO;
+import back_end.dao.DAOFactory;
+import back_end.dao.custom.CustomerDAO;
+import back_end.dto.UserDTO;
+
+public class UserBOImpl implements UserBO {
+    CustomerDAO dao = (CustomerDAO) DAOFactory.getInstace().getDAO(DAOFactory.DAOFactoryTypes.CUSTOMER);
+    @Override
+    public boolean addUser(UserDTO user) {
+        return false;
+    }
 }
