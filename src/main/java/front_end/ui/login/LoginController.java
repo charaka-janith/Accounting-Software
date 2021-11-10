8ba8b96565fdff57764be060c7302c9623479d41
dark-sl <charaka.janith.bandarawaththa@gmail.com>
2021-11-10 16:10:12 +0530

    AS-28 #comment push for mallila

--- src/main/java/front_end/ui/login/LoginController.java
+++ src/main/java/front_end/ui/login/LoginController.java
@@ -205,8 +205,8 @@ public class LoginController implements Initializable {
         } catch (FileNotFoundException e) {
 //            Theme.giveAWarning(pane, lbl_main, e.getMessage(), window_name);
         }
-        Theme.setBackgroundColor("1", btn_login);
-        Theme.setBorderColor("warning", btn_exit);
+        /*Theme.setBackgroundColor("1", btn_login);
+        Theme.setBorderColor("warning", btn_exit);*/
         /*Theme.setBackgroundColor("BG", pane);
         Theme.setBackgroundColor("1", lblMain);
         Theme.setTextFill("BG", lblMain);
