720b269bf2ebae999da7bdc150e3f1732f2bc162
dilini <linifarnando123@gmail.com>
2021-11-19 17:48:45 +0530

    AS-58 #comment Implement key events, action events on "Change Password" controller

--- src/main/java/front_end/ui/settings/ChangePasswordController.java
+++ src/main/java/front_end/ui/settings/ChangePasswordController.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 
@@ -40,49 +41,60 @@ public class ChangePasswordController {
     @FXML
     private TextField txt_newPass2;
 
-    @FXML
-    void btn_refresh_onAction(ActionEvent event) {
-
-    }
-
-    @FXML
-    void btn_save_keyReleased(KeyEvent event) {
-
-    }
-
-    @FXML
-    void btn_save_onAction(ActionEvent event) {
-
-    }
-
+    //    ..........................................Key Events........................................
     @FXML
     void txt_currentPass_keyReleased(KeyEvent event) {
-
-    }
-
-    @FXML
-    void txt_currentPass_onAction(ActionEvent event) {
-
-    }
-
-    @FXML
-    void txt_newPass2_keyReleased(KeyEvent event) {
-
-    }
-
-    @FXML
-    void txt_newPass2_onAction(ActionEvent event) {
-
+        if (event.getCode().equals(KeyCode.ESCAPE)) {
+            txt_currentPass.requestFocus();
+        }
     }
 
     @FXML
     void txt_newPass_keyReleased(KeyEvent event) {
+        if (event.getCode().equals(KeyCode.ESCAPE)) {
+            txt_currentPass.requestFocus();
+        }
+    }
 
+    @FXML
+    void txt_newPass2_keyReleased(KeyEvent event) {
+        if (event.getCode().equals(KeyCode.ESCAPE)) {
+            txt_newPass.requestFocus();
+        }
+    }
+
+    @FXML
+    void btn_save_keyReleased(KeyEvent event) {
+        if (event.getCode().equals(KeyCode.ESCAPE)) {
+            txt_newPass2.requestFocus();
+        }
+    }
+
+    //    ..........................................Action Events........................................
+    @FXML
+    void txt_currentPass_onAction(ActionEvent event) {
+        txt_newPass.requestFocus();
     }
 
     @FXML
     void txt_newPass_onAction(ActionEvent event) {
-
+        txt_newPass2.requestFocus();
     }
 
+    @FXML
+    void txt_newPass2_onAction(ActionEvent event) {
+//..........TODO-> Call Save password method
+    }
+
+    @FXML
+    void btn_save_onAction(ActionEvent event) {
+//..........TODO-> Call Save password method
+    }
+
+    @FXML
+    void btn_refresh_onAction(ActionEvent event) {
+        txt_currentPass.setText("");
+        txt_newPass.setText("");
+        txt_newPass2.setText("");
+    }
 }
