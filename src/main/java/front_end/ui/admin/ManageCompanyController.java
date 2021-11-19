8d10fe1fe30defe99816501b188d29baa412ce79
dilini <linifarnando123@gmail.com>
2021-11-19 16:04:38 +0530

    AS-57 #comment Implement key events, action events on "Manage Company" controller

--- src/main/java/front_end/ui/admin/ManageCompanyController.java
+++ src/main/java/front_end/ui/admin/ManageCompanyController.java
@@ -13,6 +13,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -22,7 +23,7 @@ import java.util.ResourceBundle;
 
 public class ManageCompanyController implements Initializable {
 
-    CompanyBO bo = (CompanyBO) BOFactory.getInstance().getBO(BOFactory.BOTypes.COMPANY);
+    CompanyBO companyBO = (CompanyBO) BOFactory.getInstance().getBO(BOFactory.BOTypes.COMPANY);
 
     @FXML
     private JFXButton btn_save;
@@ -78,89 +79,125 @@ public class ManageCompanyController implements Initializable {
     @FXML
     private TextField txt_website;
 
-    @FXML
-    void btn_save_keyReleased(KeyEvent event) {
-
-    }
-
-    @FXML
-    void btn_save_onAction(ActionEvent event) {
-
-    }
-
-    @FXML
-    void btn_refresh_onAction(ActionEvent event) {
-
-    }
-
-    @FXML
-    void txt_address_keyReleased(KeyEvent event) {
-
-    }
-
-    @FXML
-    void txt_address_onAction(MouseEvent event) {
-
-    }
-
-    @FXML
-    void txt_businessRegistrationNumber_keyReleased(KeyEvent event) {
-
-    }
-
-    @FXML
-    void txt_businessRegistrationNumber_onAction(ActionEvent event) {
-
-    }
-
-    @FXML
-    void txt_email_keyReleased(KeyEvent event) {
-
-    }
-
-    @FXML
-    void txt_email_onAction(ActionEvent event) {
-
-    }
-
+    //    ..........................................Key Events........................................
     @FXML
     void txt_name_keyReleased(KeyEvent event) {
-
-    }
-
-    @FXML
-    void txt_name_onAction(ActionEvent event) {
-
-    }
-
-    @FXML
-    void txt_phoneNumber_keyReleased(KeyEvent event) {
-
-    }
-
-    @FXML
-    void txt_phoneNumber_onAction(ActionEvent event) {
-
+        if (event.getCode().equals(KeyCode.ESCAPE)) {
+            txt_name.requestFocus();
+        }
     }
 
     @FXML
     void txt_userName_keyReleased(KeyEvent event) {
-
+        if (event.getCode().equals(KeyCode.ESCAPE)) {
+            txt_name.requestFocus();
+        }
     }
 
     @FXML
-    void txt_userName_onAction(ActionEvent event) {
+    void txt_address_keyReleased(KeyEvent event) {
+        if (event.getCode().equals(KeyCode.ESCAPE)) {
+            txt_userName.requestFocus();
+        }
+    }
 
+    @FXML
+    void txt_phoneNumber_keyReleased(KeyEvent event) {
+        if (event.getCode().equals(KeyCode.ESCAPE)) {
+            txt_address.requestFocus();
+        }
+    }
+
+    @FXML
+    void txt_email_keyReleased(KeyEvent event) {
+        if (event.getCode().equals(KeyCode.ESCAPE)) {
+            txt_phoneNumber.requestFocus();
+        }
     }
 
     @FXML
     void txt_website_keyReleased(KeyEvent event) {
+        if (event.getCode().equals(KeyCode.ESCAPE)) {
+            txt_email.requestFocus();
+        }
+    }
 
+    @FXML
+    void txt_businessRegistrationNumber_keyReleased(KeyEvent event) {
+        if (event.getCode().equals(KeyCode.ESCAPE)) {
+            txt_website.requestFocus();
+        }
+    }
+
+    @FXML
+    void btn_save_keyReleased(KeyEvent event) {
+        if (event.getCode().equals(KeyCode.ESCAPE)) {
+            txt_businessRegistrationNumber.requestFocus();
+        }
+    }
+
+    //    ..........................................Action Events........................................
+    @FXML
+    void txt_name_onAction(ActionEvent event) {
+        txt_userName.requestFocus();
+    }
+
+    @FXML
+    void txt_userName_onAction(ActionEvent event) {
+        txt_address.requestFocus();
+    }
+
+    @FXML
+    void txt_address_onAction(MouseEvent event) {
+        txt_phoneNumber.requestFocus();
+    }
+
+    @FXML
+    void txt_phoneNumber_onAction(ActionEvent event) {
+        txt_email.requestFocus();
+    }
+
+    @FXML
+    void txt_email_onAction(ActionEvent event) {
+        txt_website.requestFocus();
     }
 
     @FXML
     void txt_website_onAction(ActionEvent event) {
+        txt_businessRegistrationNumber.requestFocus();
+    }
 
+    @FXML
+    void txt_businessRegistrationNumber_onAction(ActionEvent event) {
+        //.....TODO----->Call Save or update method
+        setCompanyDetails();
+    }
+
+    @FXML
+    void btn_save_onAction(ActionEvent event) {
+        //.....TODO----->Call Save or update method
+        setCompanyDetails();
+    }
+
+    @FXML
+    void btn_refresh_onAction(ActionEvent event) {
+        setCompanyDetails();
+    }
+
+    private void setCompanyDetails() {
+        try {
+            CompanyDTO newCompany = companyBO.getCompany();
+            lbl_main.setText("Update Company");
+            btn_save.setText("Update [F1]");
+            txt_name.setText(newCompany.getName());
+            txt_address.setText(newCompany.getAddress());
+            txt_phoneNumber.setText(newCompany.getPhoneNumber());
+            txt_email.setText(newCompany.getEmail());
+            txt_website.setText(newCompany.getWebSite());
+            txt_businessRegistrationNumber.setText(newCompany.getBrn());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -173,17 +210,10 @@ public class ManageCompanyController implements Initializable {
         });
         new Thread(() -> {
             try {
-                CompanyDTO company = bo.getCompany();
+                CompanyDTO company = companyBO.getCompany();
                 if (null != company) {
                     Platform.runLater(() -> {
-                        lbl_main.setText("Update Company");
-                        btn_save.setText("Update [F1]");
-                        txt_name.setText(company.getName());
-                        txt_address.setText(company.getAddress());
-                        txt_phoneNumber.setText(company.getPhoneNumber());
-                        txt_email.setText(company.getEmail());
-                        txt_website.setText(company.getWebSite());
-                        txt_businessRegistrationNumber.setText(company.getBrn());
+                        setCompanyDetails();
                     });
                 } else {
                     Platform.runLater(() -> {
